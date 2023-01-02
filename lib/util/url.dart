1f70ed5aeb5e235786dba2cb332be2547daa9caa
shubham sapkota <shubham.sapkota1@gmail.com>
2023-01-03 00:56:54 +0545

    nearly completed

---
+++ lib/util/url.dart
@@ -0,0 +1,18 @@
+const baseUrl = "http://10.0.2.2:4000/api/v2/";
+// const baseUrl = "http://localhost:4000/api/v2/";
+// const Token =
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZThmYzQ2YzQ0YTMwYTYzY2QwYjg3YSIsImlhdCI6MTY3Nzc4MTkxNiwiZXhwIjoxNjc4MzAwMzE2fQ.m4rm1Za9Uo3er2dR3EdArGLv6lzHcoAx0StwnBudJ2I";
+
+const loginUrl = "login";
+const registerUrl = "register";
+const getProductsUrl = "products";
+const getProfileUrl = "userdetails";
+const getOrderHistoryUrl = "orders/myyy";
+const createOrderUrl = "orders/create";
+const changePasswordUrl = "me/updates";
+const getReviewUrl = "product/review";
+const potReviewUrl = "/create/product/review";
+const updateProfileUrl = "me/update/profiles";
+const createOrder = "order/flutter/create";
+
+String? token;
